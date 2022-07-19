e343d72cb9adb596bdb1def300723c30ce700a6b
web-padawan <iamkulykov@gmail.com>
2022-07-19 16:42:00 +0300

    Remove logic to disable SSR

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
-
-import dynamic from "next/dynamic";
 import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default dynamic(() => Promise.resolve(MyApp), {
-  ssr: false,
-});
+export default MyApp;
